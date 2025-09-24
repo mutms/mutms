68fb22269129cadc4b9c41ce23b88711d63e4153
Petr Skoda <petr@mutms.org>
2025-09-24 09:28:52 +0200

    Squashed 'blocks/muprog_my/' changes from 21292892a00..1ce1c65a579
    1ce1c65a579 Bump version after release
    
    git-subtree-dir: blocks/muprog_my
    git-subtree-split: 1ce1c65a57915d84a4d5bcb9604de169841aaf03

--- version.php
+++ version.php
@@ -35,7 +35,7 @@ $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->supported = [500, 500];
 
-$plugin->release = 'mu-5.0.2-02';
+$plugin->release = 'mu-5.0.2-02+';
 
 $plugin->dependencies = [
     'tool_muprog' => 2025083150,
