8cc92bac9072003676b149bdf3538575b8097d12
Petr Skoda <petr@mutms.org>
2025-09-24 09:34:51 +0200

    Squashed 'admin/tool/mutenancy/' changes from 8f9d05acb2e..73fda154875
    73fda154875 Prepare mu-5.0.2-03 release
    
    git-subtree-dir: admin/tool/mutenancy
    git-subtree-split: 73fda15487513f0d959aaa1cb293457b76791624

--- version.php
+++ version.php
@@ -28,13 +28,13 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_mutenancy';
-$plugin->version = 2025083150.01;
+$plugin->version = 2025092450;
 $plugin->requires = 2025041401;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->supported = [500, 500];
 
-$plugin->release = 'mu-5.0.2-02+';
+$plugin->release = 'mu-5.0.2-03';
 
 $plugin->dependencies = [
-    'tool_mulib' => 2025083150,
+    'tool_mulib' => 2025092450,
 ];
