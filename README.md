6f3ad95c463ae4c382aaf2a762d137c68f9c0e42
Petr Skoda <petr@mutms.org>
2025-09-28 22:17:32 +0200

    Squashed 'public/blocks/mucertify_my/' content from commit ba831e449de
    git-subtree-dir: public/blocks/mucertify_my
    git-subtree-split: ba831e449de7fc6ff0da189bf249f0e89cf7f71b

---
+++ README.md
@@ -0,0 +1,14 @@
+# My certifications block for Moodle™ LMS
+
+![Moodle Plugin CI](https://github.com/mutms/moodle-block_mucertify_my/actions/workflows/moodle-ci.yml/badge.svg)
+
+To use this block you need to install [Certifications plugin for Moodle™ LMS](https://github.com/mutms/moodle-tool_mucertify).
+
+See [online documentation](https://github.com/mutms/moodle-tool_mucertify/wiki) for more information.
+
+## Acknowledgement
+
+This plugin is a fork of [My Certifications block by Open LMS](https://github.com/open-lms-open-source/moodle-block_mycertify)
+and exists thanks to Open LMS's decision to release it to the public under the GPL 3.0 license.
+
+MuTMS suite of plugins is not associated with Moodle HQ or Open LMS in any way.
---
+++ tests/phpunit/block_test.php
@@ -0,0 +1,59 @@
+<?php
+// This file is part of MuTMS suite of plugins for Moodle™ LMS.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+// phpcs:disable moodle.Files.BoilerplateComment.CommentEndedTooSoon
+
+namespace block_mucertify_my\phpunit;
+
+/**
+ * My certifications overview block tests.
+ *
+ * @package     block_mucertify_my
+ * @copyright   2025 Petr Skoda
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+final class block_test extends \advanced_testcase {
+    public static function setUpBeforeClass(): void {
+        require_once(__DIR__ . '/../../../moodleblock.class.php');
+        require_once(__DIR__ . '/../../block_mucertify_my.php');
+        parent::setUpBeforeClass();
+    }
+
+    /**
+     * Test the behaviour of can_block_be_added() method.
+     *
+     * @covers ::can_block_be_added
+     */
+    public function test_can_block_be_added(): void {
+        $this->resetAfterTest();
+        $this->setAdminUser();
+
+        // Create a course and prepare the page where the block will be added.
+        $course = $this->getDataGenerator()->create_course();
+        $page = new \moodle_page();
+        $page->set_context(\context_course::instance($course->id));
+        $page->set_pagelayout('course');
+
+        $block = new \block_mucertify_my();
+
+        $this->assertFalse($block->can_block_be_added($page));
+
+        /** @var \tool_mucertify_generator $generator */
+        $generator = $this->getDataGenerator()->get_plugin_generator('tool_mucertify');
+        $generator->create_certification();
+        $this->assertTrue($block->can_block_be_added($page));
+    }
+}
