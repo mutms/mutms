aa768c7c4d767c39a26fcf908bda013e82589149
Petr Skoda <petr@mutms.org>
2025-10-06 10:08:53 +0200

    Squashed 'public/blocks/mucertify_my/' changes from ba831e449de..e12df914cdf
    e12df914cdf Finalise mu-5.0.3-01 release
    
    git-subtree-dir: public/blocks/mucertify_my
    git-subtree-split: e12df914cdfca0ef90c3e62db02b362f35fa9d8f

--- version.php
+++ version.php
@@ -30,13 +30,13 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'block_mucertify_my';
-$plugin->version = 2025092450;
+$plugin->version = 2025100650;
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->supported = [500, 501];
 
-$plugin->release = 'mu-5.0.2-03+';
+$plugin->release = 'mu-5.0.3-01';
 
 $plugin->dependencies = [
-    'tool_mucertify' => 2025092450,
+    'tool_mucertify' => 2025100650,
 ];
