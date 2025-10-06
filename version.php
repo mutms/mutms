d4eb4325b4e63696f4ef4401d6bc110f3828a05d
Petr Skoda <petr@mutms.org>
2025-10-06 10:05:32 +0200

    Squashed 'admin/tool/mutenancy/' changes from 73fda154875..7839f8f005d
    7839f8f005d Finalise mu-5.0.3-01 release
    c52d674a229 Prepare mu-5.0.3-01 release
    064f4da18ed Prevent use with Moodle 5.1
    6cab66771dd Bump release
    
    git-subtree-dir: admin/tool/mutenancy
    git-subtree-split: 7839f8f005d697cac8f89d0638263b70d0969339

--- version.php
+++ version.php
@@ -28,13 +28,13 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_mutenancy';
-$plugin->version = 2025092450;
+$plugin->version = 2025100650;
 $plugin->requires = 2025041401;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->supported = [500, 500];
-
-$plugin->release = 'mu-5.0.2-03';
+$plugin->incompatible = 501;
+$plugin->release = 'mu-5.0.3-01';
 
 $plugin->dependencies = [
-    'tool_mulib' => 2025092450,
+    'tool_mulib' => 2025100650,
 ];
