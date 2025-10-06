d0677519f95f26e17b0bc7347c13ef41f0062cd2
Petr Skoda <petr@mutms.org>
2025-10-06 10:08:34 +0200

    Squashed 'public/blocks/muprog_my/' changes from 58c0c4119ee..7f409a99e3c
    7f409a99e3c Finalise mu-5.0.3-01 release
    
    git-subtree-dir: public/blocks/muprog_my
    git-subtree-split: 7f409a99e3cad8ed6f12a24b13b9a3aa9898f3df

--- version.php
+++ version.php
@@ -30,13 +30,13 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'block_muprog_my';
-$plugin->version = 2025092450;
+$plugin->version = 2025100650;
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->supported = [500, 501];
 
-$plugin->release = 'mu-5.0.2-03+';
+$plugin->release = 'mu-5.0.3-01';
 
 $plugin->dependencies = [
-    'tool_muprog' => 2025092450,
+    'tool_muprog' => 2025100650,
 ];
