28eb86dc8c2cdaaba0502041da44b2dd63b223c5
Petr Skoda <petr@mutms.org>
2025-11-08 10:37:31 +0100

    Squashed 'admin/tool/certificate/element/mucertify/' changes from 10fef1a14b6..74060474e1a
    74060474e1a Prepare mu-5.0.3-02 release
    5f4f7f9d2bd Bump release
    
    git-subtree-dir: admin/tool/certificate/element/mucertify
    git-subtree-split: 74060474e1a50a11e1ba4fb824bf70a9c3d5b6e5

--- version.php
+++ version.php
@@ -28,14 +28,14 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'certificateelement_mucertify';
-$plugin->version = 2025100650;
+$plugin->version = 2025110850;
 $plugin->requires = 2025041400;
-$plugin->maturity = MATURITY_ALPHA;
+$plugin->maturity = MATURITY_BETA;
 $plugin->supported = [500, 501];
 
-$plugin->release = 'mu-5.0.3-01';
+$plugin->release = 'mu-5.0.3-02';
 
 $plugin->dependencies = [
-    'tool_mucertify' => 2025100650,
+    'tool_mucertify' => 2025110850,
     'tool_certificate' => 2025041400,
 ];
