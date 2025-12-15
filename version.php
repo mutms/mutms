a461559ceb4d185154df35f897e5d97b604a06b9
Petr Skoda <petr@mutms.org>
2025-12-15 23:55:34 +0100

    Squashed 'blocks/muprog_my/' changes from 8ebb7dcd348..91be286dacc
    91be286dacc Prepare release
    ace54f68e81 Add test group
    23a6a46b577 Use new helper
    2b7108fadac Back to dev
    
    git-subtree-dir: blocks/muprog_my
    git-subtree-split: 91be286dacc53e4f3448bc20384143105b8617f9

--- version.php
+++ version.php
@@ -30,13 +30,13 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'block_muprog_my';
-$plugin->version = 2025120850;
+$plugin->version = 2025121650;
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_BETA;
 $plugin->supported = [500, 501];
 
-$plugin->release = 'mu-5.0.4-01';
+$plugin->release = 'mu-5.0.4-02';
 
 $plugin->dependencies = [
-    'tool_muprog' => 2025120850,
+    'tool_muprog' => 2025121650,
 ];
