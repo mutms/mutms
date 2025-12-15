3b74345fcbbf172963dfd78cbb157b9d5f9dd877
Petr Skoda <petr@mutms.org>
2025-12-15 23:55:45 +0100

    Squashed 'blocks/mucertify_my/' changes from e06dac9c438..86e6477aa8c
    86e6477aa8c Prepare release
    6e530ccdbb4 Add test group
    b319d15d0b3 Back to dev
    
    git-subtree-dir: blocks/mucertify_my
    git-subtree-split: 86e6477aa8ce5adf4c8496ed99f2a3c83ecb8711

--- version.php
+++ version.php
@@ -30,13 +30,13 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'block_mucertify_my';
-$plugin->version = 2025120850;
+$plugin->version = 2025121650;
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_BETA;
 $plugin->supported = [500, 501];
 
-$plugin->release = 'mu-5.0.4-01';
+$plugin->release = 'mu-5.0.4-02';
 
 $plugin->dependencies = [
-    'tool_mucertify' => 2025120850,
+    'tool_mucertify' => 2025121650,
 ];
