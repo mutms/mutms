bba61d760ab74ae3839bdcabd60fd5ac7ad8587a
Petr Skoda <petr@mutms.org>
2025-12-16 00:03:00 +0100

    Squashed 'admin/tool/murelation/' changes from 02cbcd0a564..e9774b93a3a
    e9774b93a3a Prepare release
    3890185966c Prepare release
    41495a0ede2 Back to dev
    
    git-subtree-dir: admin/tool/murelation
    git-subtree-split: e9774b93a3a3333cf69aa4d4f9b12331edce2d4a

--- version.php
+++ version.php
@@ -28,13 +28,13 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_murelation';
-$plugin->version = 2025120850;
+$plugin->version = 2025121650;
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_BETA;
 $plugin->supported = [500, 501];
 
-$plugin->release = 'mu-5.0.4-01';
+$plugin->release = 'mu-5.0.4-02';
 
 $plugin->dependencies = [
-    'tool_mulib' => 2025120850,
+    'tool_mulib' => 2025121650,
 ];
