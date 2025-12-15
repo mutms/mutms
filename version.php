49f03cec36ebc05f00e205e0338facb076866f52
Petr Skoda <petr@mutms.org>
2025-12-15 23:56:50 +0100

    Squashed 'public/admin/tool/mutenancy/' changes from ea87ad14f0a..634ad487ff0
    634ad487ff0 Prepare release
    a1b185b3630 Back to dev
    
    git-subtree-dir: public/admin/tool/mutenancy
    git-subtree-split: 634ad487ff0f4d16ed6bfeaffdd2a2a12592a1c1

--- version.php
+++ version.php
@@ -28,13 +28,13 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_mutenancy';
-$plugin->version = 2025120851;
+$plugin->version = 2025121651;
 $plugin->requires = 2025100601;
 $plugin->maturity = MATURITY_BETA;
 $plugin->supported = [501, 501];
 $plugin->incompatible = 502;
-$plugin->release = 'mu-5.1.1-01';
+$plugin->release = 'mu-5.1.1-02';
 
 $plugin->dependencies = [
-    'tool_mulib' => 2025120850,
+    'tool_mulib' => 2025121650,
 ];
