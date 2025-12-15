1223ddeaadeb32666c780e40d97f7b708b3024c2
Petr Skoda <petr@mutms.org>
2025-12-15 23:55:15 +0100

    Squashed 'admin/tool/certificate/' changes from 56fc264969a..52a9c039363
    52a9c039363 Merge branch '5.1.1_release' into 'MOODLE_500_STABLE'
    b312586d80c Version bump 5.0.4
    e3d88de9760 Merge branch '5.0.4-changelog' into 'MOODLE_500_STABLE'
    aeb81631422 Pending change log updates for 5.0.4.
    1b9b4b9e769 Merge branch 'WPOS-185' into 'MOODLE_500_STABLE'
    da97258081a WPOS-185 Add missing type and default for details form
    70f3a30c9d4 Merge branch 'WP-5747' into 'MOODLE_500_STABLE'
    9be7e12b3ab WP-5747 adhere to latest codechecker coding style rules.
    6bacbb0e9aa Merge branch 'WPOS-181' into 'MOODLE_500_STABLE'
    f3475dd2dbe WPOS-181 remove defunct conditional version checks.
    b52fa4adfd2 Merge branch 'WP-5704' into 'MOODLE_500_STABLE'
    0f88c547616 WP-5704: Update date and time handling to use the Clock API
    702c37623af Merge branch 'WP-5005' into 'MOODLE_500_STABLE'
    be201eada0b Merge branch 'WPOS-175' into 'MOODLE_500_STABLE'
    0bfff20462a Merge branch 'WP-4799' into 'MOODLE_500_STABLE'
    309fcfb8190 WP-5005 switch to Selenium 4 Chrome container.
    000e59198e2 WP-4799 switch to simpler Gitlab version matrix.
    70abe8029b4 WPOS-175 make bulk certificate action reusable
    fba94d7cb9e Merge branch '5.1-beta' into 'MOODLE_500_STABLE'
    aa48120b71e Version bump post-5.1 release.
    eabafd2c2b6 WPOS-38 revert removal of required group element selector.
    cc88728362f Merge branch 'WPOS-38-2' into 'MOODLE_500_STABLE'
    124e8504b73 WPOS-38 revert circular Tenant branch dependency.
    7a4b3745f8d Merge branch 'WPOS-38' into 'MOODLE_500_STABLE'
    2bf5bd7f2bd WPOS-38 Adds bulk certificate regeneration with notification option
    
    git-subtree-dir: admin/tool/certificate
    git-subtree-split: 52a9c039363136b937fc90dd85e9f140c39aacfb

--- certificates.php
+++ certificates.php
@@ -23,7 +23,7 @@
  */
 
 require_once('../../../config.php');
-require_once($CFG->libdir.'/adminlib.php');
+require_once($CFG->libdir . '/adminlib.php');
 
 $download = optional_param('download', null, PARAM_ALPHA);
 $templateid = required_param('templateid', PARAM_INT);
@@ -48,8 +48,11 @@ if (!$template->can_view_issues()) {
 
 $heading = $title = $template->get_formatted_name();
 if ($template->get_shared()) {
-    $heading .= html_writer::tag('div', get_string('shared', 'tool_certificate'),
-        ['class' => 'badge rounded-pill bg-secondary text-dark font-small ms-2 align-middle']);
+    $heading .= html_writer::tag(
+        'div',
+        get_string('shared', 'tool_certificate'),
+        ['class' => 'badge rounded-pill bg-secondary text-dark font-small ms-2 align-middle']
+    );
 }
 $PAGE->navbar->add($title, $pageurl);
 $PAGE->set_title($title);
@@ -70,6 +73,8 @@ if ($template->can_issue_to_anybody()) {
         'addbuttonicon' => true, ];
 }
 $PAGE->requires->js_call_amd('tool_certificate/issues-list', 'init');
+
+$data['bulkactionsselect'] = true;
 
 echo $OUTPUT->header();
 echo $OUTPUT->render_from_template('tool_certificate/content_with_heading', $data);
