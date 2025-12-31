fe1932819033aee31ae1a3aae9192d9a4474f32f
Petr Skoda <petr@mutms.org>
2025-12-31 11:27:05 +0100

    Squashed 'admin/tool/mutrain/' changes from f8bf24f1fc4..9c5b23914c7
    9c5b23914c7 Revert versioning changes
    
    git-subtree-dir: admin/tool/mutrain
    git-subtree-split: 9c5b23914c7c4d16ed913966f59a03f69fc97a19

--- version.php
+++ version.php
@@ -28,7 +28,7 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_mutrain';
-$plugin->version = '2025123150';
+$plugin->version = 2025123150;
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_BETA;
 $plugin->supported = [500, 501];
@@ -36,6 +36,6 @@ $plugin->supported = [500, 501];
 $plugin->release = 'mu-5.0.4-03';
 
 $plugin->dependencies = [
-    'tool_mulib' => '2025123150',
-    'customfield_mutrain' => '2025123150',
+    'tool_mulib' => 2025123150,
+    'customfield_mutrain' => 2025123150,
 ];
