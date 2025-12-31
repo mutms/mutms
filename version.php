fea6329d9e550599bd5815a424a376cd8d7b44ee
Petr Skoda <petr@mutms.org>
2025-12-31 11:27:51 +0100

    Squashed 'public/admin/tool/mutenancy/' changes from bd7f32443e2..d755a86ba5f
    d755a86ba5f Revert versioning changes
    
    git-subtree-dir: public/admin/tool/mutenancy
    git-subtree-split: d755a86ba5f07e727a6512d795e2db756de6e632

--- version.php
+++ version.php
@@ -28,7 +28,7 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_mutenancy';
-$plugin->version = '2025123151';
+$plugin->version = 2025123151;
 $plugin->requires = 2025100601;
 $plugin->maturity = MATURITY_BETA;
 $plugin->supported = [501, 501];
@@ -36,5 +36,5 @@ $plugin->incompatible = 502;
 $plugin->release = 'mu-5.1.1-03';
 
 $plugin->dependencies = [
-    'tool_mulib' => '2025123150',
+    'tool_mulib' => 2025123150,
 ];
