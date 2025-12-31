2878fb654471f17942a8cef5e619e756227e8382
Petr Skoda <petr@mutms.org>
2025-12-31 11:27:15 +0100

    Squashed 'admin/tool/mupwned/' changes from 23ec688608c..0f4e0219048
    0f4e0219048 Revert versioning changes
    
    git-subtree-dir: admin/tool/mupwned
    git-subtree-split: 0f4e0219048a0a6b3319b9f4f0e2848c65b301bf

--- version.php
+++ version.php
@@ -28,7 +28,7 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_mupwned';
-$plugin->version = '2025123150';
+$plugin->version = 2025123150;
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_BETA;
 $plugin->supported = [500, 501];
