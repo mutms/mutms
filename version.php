275d8fb7920342326d02d2ff5dbfc7406a60552f
Petr Skoda <petr@mutms.org>
2025-12-31 11:26:43 +0100

    Squashed 'blocks/muprog_my/' changes from 597ca277278..c2760ae1776
    c2760ae1776 Revert versioning changes
    
    git-subtree-dir: blocks/muprog_my
    git-subtree-split: c2760ae1776f8872c26bf167c2992bb59fa58531

--- version.php
+++ version.php
@@ -30,7 +30,7 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'block_muprog_my';
-$plugin->version = '2025123150';
+$plugin->version = 2025123150;
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_BETA;
 $plugin->supported = [500, 501];
@@ -38,5 +38,5 @@ $plugin->supported = [500, 501];
 $plugin->release = 'mu-5.0.4-03';
 
 $plugin->dependencies = [
-    'tool_mulib' => '2025123150',
+    'tool_mulib' => 2025123150,
 ];
