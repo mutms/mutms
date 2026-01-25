5c0e359eee5e310b87482e8d1ac29174c0da39d9
Petr Skoda <petr@mutms.org>
2026-01-25 11:24:28 +0100

    Squashed 'admin/tool/muloginas/' changes from 4b58d9e8802..1a9e7ef15a6
    1a9e7ef15a6 Prepare release
    555a9c12df7 Back to dev
    
    git-subtree-dir: admin/tool/muloginas
    git-subtree-split: 1a9e7ef15a60b33c498d1b4078c199e36ffde4f0

--- version.php
+++ version.php
@@ -28,9 +28,9 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_muloginas';
-$plugin->version = 2025123150;
+$plugin->version = 2026012550;
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_BETA;
 $plugin->supported = [500, 501];
 
-$plugin->release = 'mu-5.0.4-03';
+$plugin->release = 'mu-5.0.4-04';
