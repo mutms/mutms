2e964f448391acc16d96a78a18dde99749b8dcfe
Petr Skoda <petr@mutms.org>
2026-01-25 11:26:44 +0100

    Squashed 'public/mod/mubook/' changes from 8cf4dd21ba4..251692dec5a
    251692dec5a Prepare release
    0cd50186b79 Update required libraries
    ae425f46a9a Back to dev
    
    git-subtree-dir: public/mod/mubook
    git-subtree-split: 251692dec5a363e84fe42c6180b7f247cd785b27

--- version.php
+++ version.php
@@ -28,12 +28,12 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'mod_mubook';
-$plugin->version = 2025123150;
+$plugin->version = 2026012550;
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_BETA;
 $plugin->supported = [500, 501];
-$plugin->release = 'mu-5.0.4-03';
+$plugin->release = 'mu-5.0.4-04';
 
 $plugin->dependencies = [
-    'tool_mulib' => 2025123150,
+    'tool_mulib' => 2026012550,
 ];
