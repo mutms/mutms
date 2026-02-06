774664829f44b1fcb7e0cb6e65cdba173b661f96
Petr Skoda <petr@mutms.org>
2026-02-06 10:30:03 +0100

    Squashed 'admin/tool/certificate/element/muprog/' changes from a1096bc7d4c..20bd533482d
    20bd533482d Prepare release
    ef8c2eed65b Back to dev
    
    git-subtree-dir: admin/tool/certificate/element/muprog
    git-subtree-split: 20bd533482dee4e296f792b367e611d3f5f7c678

--- version.php
+++ version.php
@@ -28,14 +28,14 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'certificateelement_muprog';
-$plugin->version = 2026012550;
+$plugin->version = 2026020850;
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_BETA;
 $plugin->supported = [500, 501];
 
-$plugin->release = 'mu-5.0.4-04';
+$plugin->release = 'mu-5.0.5-01';
 
 $plugin->dependencies = [
-    'tool_muprog' => 2026012550,
+    'tool_muprog' => 2026020850,
     'tool_certificate' => 2025041400,
 ];
