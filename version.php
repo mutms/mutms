1d6ca998a4265d8ddc3c1b59c979f5d96ea58be7
Petr Skoda <petr@mutms.org>
2026-02-06 10:33:09 +0100

    Squashed 'public/admin/tool/murelation/' changes from 92b4247763d..9772b188209
    9772b188209 Prepare release
    e8034e12783 Remove annoying check
    973a49dc653 Back to dev
    
    git-subtree-dir: public/admin/tool/murelation
    git-subtree-split: 9772b1882099b6647b665be0152f3bf460b25a77

--- version.php
+++ version.php
@@ -28,13 +28,13 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_murelation';
-$plugin->version = 2026012550;
+$plugin->version = 2026020850;
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_BETA;
 $plugin->supported = [500, 501];
 
-$plugin->release = 'mu-5.0.4-04';
+$plugin->release = 'mu-5.0.5-01';
 
 $plugin->dependencies = [
-    'tool_mulib' => 2026012550,
+    'tool_mulib' => 2026020850,
 ];
