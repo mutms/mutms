bae6dfed5f4184818116ae33a70c4cafd039671a
Petr Skoda <petr@mutms.org>
2026-02-12 20:41:34 +0100

    Squashed 'public/admin/tool/muloginas/' changes from ca17afc930f..6476b0122e2
    6476b0122e2 Prepare release
    07309fd57fd Bump version after release
    
    git-subtree-dir: public/admin/tool/muloginas
    git-subtree-split: 6476b0122e226ea830bc5fca850c7f697cff4174

--- version.php
+++ version.php
@@ -28,9 +28,9 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_muloginas';
-$plugin->version = 2026020850;
+$plugin->version = 2026021250;
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_BETA;
 $plugin->supported = [500, 501];
 
-$plugin->release = 'mu-5.0.5-01';
+$plugin->release = 'v5.0.6.01';
