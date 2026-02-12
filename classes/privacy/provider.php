6a3caa6de82acc699d77849d3856ac4d1d40465f
Petr Skoda <petr@mutms.org>
2026-02-12 20:41:31 +0100

    Squashed 'public/admin/tool/mutrain/' changes from a3c2b8e6fda..6b777028a94
    6b777028a94 Prepare release
    91932a1189f Fix CI
    bcb97f57956 Bump version after release
    
    git-subtree-dir: public/admin/tool/mutrain
    git-subtree-split: 6b777028a9483c087d983864b7fb1e669d04605c

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -15,7 +15,6 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 // phpcs:disable moodle.Files.BoilerplateComment.CommentEndedTooSoon
-// phpcs:disable Universal.OOStructures.AlphabeticExtendsImplements
 
 namespace tool_mutrain\privacy;
 
