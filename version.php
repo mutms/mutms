7c7a215ecc1f80ca029cce01bf06ab69e737172c
Petr Skoda <petr@mutms.org>
2026-02-12 20:39:45 +0100

    Squashed 'admin/tool/mucertify/' changes from 03738cd791c..947cdadae39
    947cdadae39 Prepare release
    802a4b298e6 Fix CI
    8ee6002d20a Bump version after release
    
    git-subtree-dir: admin/tool/mucertify
    git-subtree-split: 947cdadae39f21f0a93eb58437897bcff20bd56b

--- version.php
+++ version.php
@@ -30,15 +30,15 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_mucertify';
-$plugin->version = 2026020850;
+$plugin->version = 2026021250;
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_BETA;
 $plugin->supported = [500, 501];
 
-$plugin->release = 'mu-5.0.5-01';
+$plugin->release = 'v5.0.6.01';
 
 $plugin->dependencies = [
-    'tool_mulib' => 2026020850,
-    'tool_muprog' => 2026020850,
-    'block_mucertify_my' => 2026020850,
+    'tool_mulib' => 2026021250,
+    'tool_muprog' => 2026021250,
+    'block_mucertify_my' => 2026021250,
 ];
