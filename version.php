1bd56adf717a722fc684df809cab063a4bda883c
Petr Skoda <petr@mutms.org>
2026-03-01 09:25:40 +0100

    Squashed 'admin/tool/muhome/' changes from bc8c765fbd6..8f64a70a467
    8f64a70a467 Prepare release
    96826702563 Improve changelog
    072ca180a2c Improve CI
    71cac433cfd Back to dev
    
    git-subtree-dir: admin/tool/muhome
    git-subtree-split: 8f64a70a4677d2d0b38574534a2c8b8dd81c3358

--- version.php
+++ version.php
@@ -28,12 +28,11 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_muhome';
-$plugin->version = 2026021250;
+$plugin->version = 2026030150;
 $plugin->requires = 2024100700;
-$plugin->maturity = MATURITY_BETA;
 $plugin->supported = [500, 501];
-$plugin->release = 'v5.0.6.01';
+$plugin->release = 'v5.0.6.02';
 
 $plugin->dependencies = [
-    'tool_mulib' => 2026021250,
+    'tool_mulib' => 2026030150,
 ];
