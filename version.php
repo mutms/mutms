50ade86bb6f5d35dfd265e22dae0bfa945457c27
Petr Skoda <petr@mutms.org>
2026-03-25 19:25:25 +0100

    Squashed 'blocks/muprog_my/' changes from f3aeac01ee8..1089ce56516
    1089ce56516 Prepare release
    ae0a8209680 Prepare release
    31e2a1b6897 Update docs link
    9e860279aef Fix docs link
    e903402ca14 Back to dev
    
    git-subtree-dir: blocks/muprog_my
    git-subtree-split: 1089ce565160b9d8b4f7206b3159887ef7e9de0c

--- version.php
+++ version.php
@@ -30,12 +30,12 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'block_muprog_my';
-$plugin->version = 2026030150;
+$plugin->version = 2026032650;
 $plugin->requires = 2025041400;
 $plugin->supported = [500, 501];
 
-$plugin->release = 'v5.0.6.02';
+$plugin->release = 'v5.0.6.03';
 
 $plugin->dependencies = [
-    'tool_mulib' => 2026030150,
+    'tool_mulib' => 2026032650,
 ];
