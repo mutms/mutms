ff9b58a217fee42c299e6be02635aaea5162c8c2
Petr Skoda <petr@mutms.org>
2026-03-28 14:46:32 +0100

    Squashed 'public/admin/tool/mutenancy/' content from commit e3c109fea4b
    git-subtree-dir: public/admin/tool/mutenancy
    git-subtree-split: e3c109fea4b7cb386ca64331f171f0ec9890a1d8

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Change log
+
+Plugin versioning is derived from Moodle releases, it does not comply with the semantic versioning standard.
+
+The format of this change log follows the advice given at [Keep a CHANGELOG](https://keepachangelog.com).
+
+## [Unreleased](https://github.com/mutms/moodle-tool_mutenancy/compare/v5.2.0.00...MOODLE_501_STABLE)
+
+- Initial release for Moodle 5.2
---
+++ version.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of MuTMS suite of plugins for Moodle™ LMS.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+// phpcs:disable moodle.Files.BoilerplateComment.CommentEndedTooSoon
+
+/**
+ * Multi-tenancy plugin version.
+ *
+ * @package     tool_mutenancy
+ * @copyright   2025 Petr Skoda
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/** @var stdClass $plugin */
+$plugin->component = 'tool_mutenancy';
+$plugin->version = 2026032852;
+$plugin->requires = 2026032000;
+$plugin->supported = [502, 502];
+$plugin->incompatible = 503;
+$plugin->release = 'v5.2.0.00';
+
+$plugin->dependencies = [
+    'tool_mulib' => 2026032850,
+];
