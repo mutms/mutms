b6470c592ad1e2efa64d4a9da8882af715563f28
Petr Skoda <petr@mutms.org>
2026-03-28 14:28:53 +0100

    Squashed 'admin/tool/certificate/element/muprog/' changes from b14ecb64d96..de34c3891ac
    de34c3891ac Prepare release
    9ca59bbdc65 Switch to new Moodle 5.1 composer
    2e54f0a9418 Back to dev
    
    git-subtree-dir: admin/tool/certificate/element/muprog
    git-subtree-split: de34c3891ace0c6393140562604ac6b887fef296

--- version.php
+++ version.php
@@ -28,13 +28,13 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'certificateelement_muprog';
-$plugin->version = 2026032750;
+$plugin->version = 2026032850;
 $plugin->requires = 2025041400;
 $plugin->supported = [500, 502];
 
-$plugin->release = 'v5.0.6.04';
+$plugin->release = 'v5.0.6.05';
 
 $plugin->dependencies = [
-    'tool_muprog' => 2026032750,
+    'tool_muprog' => 2026032850,
     'tool_certificate' => 2025041400,
 ];
