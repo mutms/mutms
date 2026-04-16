d0cd86881e6d7b683587abac71d64ea8a2716b61
Petr Skoda <petr@mutms.org>
2026-04-16 11:56:07 +0200

    Squashed 'mod/mubook/' changes from f8112d19082..109fd1fec06
    109fd1fec06 Back to dev
    ac0ffa68e63 Ignore LLM for now
    
    git-subtree-dir: mod/mubook
    git-subtree-split: 109fd1fec06dc7afd8784957a0f61f76a8810ee5

--- version.php
+++ version.php
@@ -31,7 +31,7 @@ $plugin->component = 'mod_mubook';
 $plugin->version = 2026032950;
 $plugin->requires = 2025041400;
 $plugin->supported = [500, 502];
-$plugin->release = 'v5.0.6.06';
+$plugin->release = 'v5.0.6.06+';
 
 $plugin->dependencies = [
     'tool_mulib' => 2026032950,
