a534b62159ce66ea15c779b94d874112a26fbd51
Petr Skoda <petr@mutms.org>
2026-04-16 11:56:01 +0200

    Squashed 'admin/tool/mupwned/' changes from 2dab3a04187..91add5361d6
    91add5361d6 Back to dev
    3061683a457 Ignore LLM for now
    
    git-subtree-dir: admin/tool/mupwned
    git-subtree-split: 91add5361d64f540c51e25717daaff477d7361a4

--- version.php
+++ version.php
@@ -31,4 +31,4 @@ $plugin->component = 'tool_mupwned';
 $plugin->version = 2026032950;
 $plugin->requires = 2025041400;
 $plugin->supported = [500, 502];
-$plugin->release = 'v5.0.6.06';
+$plugin->release = 'v5.0.6.06+';
