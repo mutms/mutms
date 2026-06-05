c1f94320396e387d102e5c5e96d592a8fe0dd460
Petr Skoda <petr@mutms.org>
2026-06-05 12:45:55 +0200

    Squashed 'public/admin/tool/mutrain/' changes from 5e180058d57..49b2783f55f
    49b2783f55f Prepare v5.0.8.01 release
    
    git-subtree-dir: public/admin/tool/mutrain
    git-subtree-split: 49b2783f55fc66b1d75f53c2cf5388fd8ca07baa

--- version.php
+++ version.php
@@ -28,13 +28,13 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_mutrain';
-$plugin->version = 2026041950;
+$plugin->version = 2026060550;
 $plugin->requires = 2025041400;
 $plugin->supported = [500, 502];
 
-$plugin->release = 'v5.0.7.01';
+$plugin->release = 'v5.0.8.01';
 
 $plugin->dependencies = [
-    'tool_mulib' => 2026041950,
-    'customfield_mutrain' => 2026041950,
+    'tool_mulib' => 2026060550,
+    'customfield_mutrain' => 2026060550,
 ];
