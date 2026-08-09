b4eaf5b902323662b62985d83302cf3b73c24cbc
Petr Skoda <petr@mutms.org>
2026-08-09 21:25:31 +0200

    Squashed 'public/admin/tool/musudo/' changes from 11c1f1b6b61..23df4952b0a
    23df4952b0a Prepare release
    09b66404c51 Prepare release
    ccd7211f89f Remove obsolete release script
    7825ffa817f back to dev
    
    git-subtree-dir: public/admin/tool/musudo
    git-subtree-split: 23df4952b0a3ad8acc53aaaacb14cb265594ffcf

--- version.php
+++ version.php
@@ -28,13 +28,13 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_musudo';
-$plugin->version = 2026060550;
+$plugin->version = 2026080850;
 $plugin->requires = 2025041400;
 $plugin->supported = [500, 502];
 
-$plugin->release = 'v5.0.8.01';
+$plugin->release = 'v5.0.9.01';
 
 $plugin->dependencies = [
-    'tool_mulib' => 2026060550,
+    'tool_mulib' => 2026080850,
     'tool_mfa' => 2025041400,
 ];
