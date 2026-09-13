74969532f0779888a5f31f72b7be97a473883d43
Petr Skoda <petr@mutms.org>
2026-09-13 19:54:14 +0200

    Squashed 'mod/mubook/' changes from 2198bb938e2..83389112c40
    83389112c40 Prepare release
    2594d2b6796 Add camp info
    d1228ad7518 Back to dev
    
    git-subtree-dir: mod/mubook
    git-subtree-split: 83389112c40924d7943249d6d5246431b5ba64ca

--- version.php
+++ version.php
@@ -28,11 +28,11 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'mod_mubook';
-$plugin->version = 2026080845;
+$plugin->version = 2026091345;
 $plugin->requires = 2024100700;
 $plugin->supported = [405, 405];
-$plugin->release = 'v4.5.13.01';
+$plugin->release = 'v4.5.14.01';
 
 $plugin->dependencies = [
-    'tool_mulib' => 2026080845,
+    'tool_mulib' => 2026091345,
 ];
