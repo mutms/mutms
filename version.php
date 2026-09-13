44b816e785d6ba5d0a303d2015c9b8d7ac8011a1
Petr Skoda <petr@mutms.org>
2026-09-13 19:57:31 +0200

    Squashed 'public/admin/tool/mupwned/' changes from 6d514895aed..1d2f9c1f9df
    1d2f9c1f9df Prepare release
    e0ec5a4ccc2 Add camp info
    bbee33c42b8 Back to dev
    
    git-subtree-dir: public/admin/tool/mupwned
    git-subtree-split: 1d2f9c1f9df6b35b7fa880578fca98600b747ec7

--- version.php
+++ version.php
@@ -28,7 +28,7 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_mupwned';
-$plugin->version = 2026080850;
+$plugin->version = 2026091350;
 $plugin->requires = 2025041400;
 $plugin->supported = [500, 502];
-$plugin->release = 'v5.0.9.01';
+$plugin->release = 'v5.0.10.01';
