9afeec5a1cb71b21e1335028f3cd3a9493590337
Petr Skoda <petr@mutms.org>
2026-09-19 09:37:26 +0200

    Squashed 'admin/tool/musudo/' changes from 3742fd0c44a..a7edecf753a
    a7edecf753a Fix changelog
    9cb6c4e148a Prepare release
    b91ad7186bf Add CAMP badge
    9d2d2a06f44 Prepare release
    d203df66136 Add camp release CI
    4f769275473 Back to dev
    
    git-subtree-dir: admin/tool/musudo
    git-subtree-split: a7edecf753acd7004f92583f88ae654c646d40aa

--- version.php
+++ version.php
@@ -28,13 +28,13 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_musudo';
-$plugin->version = 2026091345;
+$plugin->version = 2026091900;
 $plugin->requires = 2024100700;
 $plugin->supported = [405, 405];
 $plugin->incompatible = 500;
-$plugin->release = 'v4.5.14.01';
+$plugin->release = 'v4.5.14.03';
 
 $plugin->dependencies = [
-    'tool_mulib' => 2026091345,
+    'tool_mulib' => 2026091900,
     'tool_mfa' => 2024100700,
 ];
