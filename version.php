021510b0e70266ff3b2485b0af84fc2aa80b9c0b
Petr Skoda <petr@mutms.org>
2026-09-19 09:39:12 +0200

    Squashed 'admin/tool/muprog/' changes from 23995c22828..18da3840100
    18da3840100 Prepare release
    8fb902a98d4 Add CAMP badge
    2581d792146 Prepare release
    417ba93ca59 Add camp release CI
    16d17361387 Back to dev
    
    git-subtree-dir: admin/tool/muprog
    git-subtree-split: 18da384010004685ec3de47531eb210feb8afe1c

--- version.php
+++ version.php
@@ -30,15 +30,15 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_muprog';
-$plugin->version = 2026091350;
+$plugin->version = 2026092000;
 $plugin->requires = 2025041400;
 $plugin->supported = [500, 502];
-
-$plugin->release = 'v5.0.10.01';
+$plugin->incompatible = 503;
+$plugin->release = 'v5.0.10.03';
 
 $plugin->dependencies = [
-    'tool_mulib' => 2026091350,
-    'enrol_muprog' => 2026091350,
-    'block_muprog_my' => 2026091350,
-    'block_muprogmyoverview' => 2026091350,
+    'tool_mulib' => 2026092000,
+    'enrol_muprog' => 2026092000,
+    'block_muprog_my' => 2026092000,
+    'block_muprogmyoverview' => 2026092000,
 ];
